f95001e853cce2787cfb1c6db1a1283a38e66924
claytonjwong <claytonjwong@gmail.com>
2017-03-26 10:33:53 -0700

    more efficient O(log n) solution by dividing by 2

--- perfect_number.py
+++ perfect_number.py
@@ -1,3 +1,23 @@
+"""
+
+507. Perfect Number My SubmissionsBack To Contest
+
+We define the Perfect Number is a positive integer that is equal
+to the sum of all its positive divisors except itself.
+
+Now, given an integer n, write a function that returns true when
+it is a perfect number and false when it is not.
+
+Example:
+Input: 28
+Output: True
+Explanation: 28 = 1 + 2 + 4 + 7 + 14
+Note: The input number n will not exceed 100,000,000. (1e8)
+
+"""
+
+import math
+
 class Solution(object):
     def checkPerfectNumber(self, num):
         """
@@ -6,25 +26,33 @@ class Solution(object):
         """
         
         #
-        # keep track of the sum of the divisors
+        # bounds check 
+        #
+        if ( num <= 0 ):
+            return False
+
+        #
+        # a perfect number is the sum of all divisors which
+        # is itself evenly divible by 2, until 1
+        # continue dividing the number by 2 and adding those values into the sum
         #
         sum = 0
-        
-
-        for i in range ( 1, int ( num / 2 ) + 1 ):
+        i = num
+       
+        while ( i > 1 ):
             
-            #
-            # if num is evenly divisible by i,
-            # then add i onto the sum
-            #
+            i = math.ceil ( i / 2 )
+            
             if ( num % i == 0 ):
                 sum += i
-        
+            else:
+                return False
+           
         #
         # if the sum of the divisors is equal to the number,
         # then return True, since this is a perfect number,
         # otherwise return False
-        #        
+        #
         if ( sum == num ):
             return True
         else:
