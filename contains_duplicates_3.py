0c13f7e335b22bc2061e2d09bc0895599a8399a9
claytonjwong <claytonjwong@gmail.com>
2017-06-06 16:22:43 -0700

    add buckets onto dup3 for efficiency

--- contains_duplicates_3.py
+++ contains_duplicates_3.py
@@ -19,38 +19,63 @@ class Solution(object):
         :type t: int
         :rtype: bool
         """
-        nearby_nums = {}
+        
+        #
+        # there is no nearby duplicate which is 0 indices away, 
+        # this would just be the same number
+        #
+        if k == 0:
+            return False
         
         #
         # compare each number within the current hash of nearby numbers
         # to see if the difference in the values is at most t
         #
+        nearby_nums = {}
         for i,curr_num in enumerate(nums):
             
-            for nearby_num_idx in nearby_nums:
-                 
-                if abs( curr_num - nearby_nums[nearby_num_idx] ) <= t:
+            #
+            # add the current number into a nearby bucket, these buckets are used as the key
+            # in the dict nearby_nums, since the difference between nums[i] and nums[j] can be
+            # at most t, divide the value of the current number by t in order to decide which
+            # bucket the number goes into, if t is 0, then the key is the number value itself
+            #
+            nearby_bucket = curr_num // t if t > 0 else curr_num
+            
+            #
+            # we only need to check the current bucket, and the bucket to the left and right
+            # in order to see if there is a number whose absolute difference with the current number
+            # is at most t
+            #
+            for nearby_bucket_idx in [nearby_bucket - 1, nearby_bucket, nearby_bucket + 1 ]:
+                
+                if  nearby_bucket_idx is not None \
+                and nearby_bucket_idx in nearby_nums \
+                and abs( curr_num - nearby_nums[nearby_bucket_idx] ) <= t:
                     return True
             
             #
-            # add entry to the hash table, the array index is used as the key,
+            # add entry to the hash table, the nearby_bucket is used as the key,
             # and the value of the current number is stored as the value
             #
-            nearby_nums[i] = curr_num
+            nearby_nums[nearby_bucket] = curr_num
             
             #
             # delete nearby num which is no longer k indices away,
             # this allows the nearby_nums hash to become a sliding window
             #
-            try:
-                del nearby_nums[i-k]
+            # calculate the index to remove as the value of the number
+            # k indices away divided by t
             #
-            # KeyError is thrown when i-k is < 0, since these entries do NOT exist
-            #
-            except KeyError:
-                pass
-        
-        
+            k_indices_away = i - k
+            
+            if ( k_indices_away >= 0 ):
+                no_longer_nearby_bucket = nums [ k_indices_away ] // t if t > 0 else nums [ k_indices_away ]
+                del nearby_nums[ no_longer_nearby_bucket ]
+
+        #
+        # no numbers match the nearby duplicate criteria
+        #
         return False
     
 
@@ -62,7 +87,7 @@ def main():
     
     solution = Solution()
     
-    print ( "True == " + str ( solution.containsNearbyAlmostDuplicate([-1,-1], 1, 0) ) )
+    print ( "True == " + str ( solution.containsNearbyAlmostDuplicate([1,3,1], 1, 1) ) )
     
 
 if __name__ == "__main__":
