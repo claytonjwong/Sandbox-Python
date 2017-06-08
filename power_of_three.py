11dfc54ffa6593159856e8a2d39fdaa7b30caee2
claytonjwong <claytonjwong@gmail.com>
2017-06-08 15:01:14 -0700

    generic power of solution

--- power_of_three.py
+++ power_of_three.py
@@ -11,7 +11,7 @@ Could you do it without using any loop / recursion?
 
 """
 
-import math
+
 
 class Solution(object):
     def isPowerOfThree(self, n):
@@ -19,34 +19,15 @@ class Solution(object):
         :type n: int
         :rtype: bool
         """
-        return self.isPowerOfX(n, 3)
-    
-    def isPowerOfX(self, n, x):
         
-        if n <= 0:
-            return False
-        
-        #
-        # find the max base power of X
-        #
-        base = x
-    
-        max_int = math.pow(2,31)
-    
-        i=2
-        while True:
+        while n > 1:
             
-            if math.pow(base, i) > max_int:
-                break
+            if n % 3 == 0:
+                n /= 3
+            else:
+                return False
             
-            i += 1
-            
-        max_base = math.pow(base, i-1)
-        
-        #
-        # n is a power of X if the max base of that power is evenly divisible by n
-        #
-        return max_base % n == 0
+        return n == 1
     
 def main():
     
