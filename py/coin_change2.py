1c9ba59e84a01ea13b5ac9a8ac96c3ffab533ec5
claytonjwong <claytonjwong@gmail.com>
2017-07-08 16:30:13 -0700

    coin change 2

--- py/coin_change2.py
+++ py/coin_change2.py
@@ -2,6 +2,8 @@
 
 518. Coin Change 2
 
+https://leetcode.com/problems/coin-change-2/#/description
+
 You are given coins of different denominations and a total amount of money.
 Write a function to compute the number of combinations that make up that amount.
 You may assume that you have infinite number of each kind of coin.
@@ -18,18 +20,52 @@ the answer is guaranteed to fit into signed 32-bit integer
 
 class Solution(object):
     def change(self, amount, coins):
-        pass
+        
+        #
+        # [0:amount]
+        #
+        dp = [0] * ( amount + 1 )
+        
+        #
+        # there is one and only one way
+        # to make change of amount 0
+        # by using NO coins at all
+        #
+        dp[0] = 1
+        
+        #
+        # find all solutions for each coin and add them up
+        # as they overlap
+        #
+        for coin in coins:
+            
+            #
+            # [1:amount]
+            #
+            for sub_amount in range(1, amount+1):
 
-
+                #
+                # += since the solutions per coin overlap
+                #
+                # i.e. for sub_amount = 2 and coins 1, 2
+                # 
+                #      for coin = 1, there is 1 solution for amount 2:
+                #      (coin 1 + coin 1)
+                #
+                #      for coin = 2, there is 1 solution for amount 2:
+                #      (coin 2)
+                #
+                if coin <= sub_amount:
+                    dp[ sub_amount ] += dp [ sub_amount - coin ]
+                
+        return dp[amount]
 
 def main():
     
     solution = Solution()
     
-    import pdb
-    pdb.set_trace()
     
-    print ( "4 == " + str ( solution.change(5, [1,2,5]) ))
+    print ( "4 == " + str ( solution.change(5, [1, 2, 5]) ))
 
 
 
